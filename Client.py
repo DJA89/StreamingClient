22004cbbc9cb0c36cdfad6c2e359fd7b356ef30d
DJA <drod89@gmail.com>
2017-09-19 08:35:50 -0300

    Add subscriber message each 30 seconds.

--- Client.py
+++ Client.py
@@ -1,16 +1,32 @@
 import socket
 import cv2
 import numpy
-
+from threading import Thread
+import time
+import sys
 UDP_IP = "127.0.0.1"
+# UDP_IP = "127.0.0.1"
 UDP_PORT = 10021
 MESSAGE = "Hello, World!"
+REFRESH_MESSAGE = "Refresh"
 
+class Subscriber(Thread):
+    def __init__(self, udp_socket):
+        Thread.__init__(self)
+        self.udp_socket = udp_socket
 
-def client():
+    def run(self):
+        while True:
+            time.sleep(30)
+            udp_socket.sendto(REFRESH_MESSAGE, (UDP_IP, UDP_PORT))
+
+if __name__ == '__main__':
     udp_socket = socket.socket(socket.AF_INET,
     socket.SOCK_DGRAM)
     udp_socket.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+    mySubscriber = Subscriber(udp_socket)
+    mySubscriber.daemon = True
+    mySubscriber.start()
     while True:
         data, address = udp_socket.recvfrom(65536)
         data = numpy.fromstring(data, dtype='uint8')
@@ -18,6 +34,3 @@ def client():
         cv2.imshow('frame',decimg)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-    while True:
-        None
-client()
